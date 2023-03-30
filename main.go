f95f9ef5d65ba2af8d36fd05fe8423bd5acf5da3
Jim Shen <3212762+sxc@users.noreply.github.com>
2023-03-30 17:16:51 +0800

    add contact page

--- main.go
+++ main.go
@@ -10,8 +10,14 @@ func handlerFunc(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprint(w, "<h1>Hello, World! This is my first Awesome and Great Go web app!</h1>")
 }
 
+func contactHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	fmt.Fprint(w, "<h1>Contact Page</h1><p>To get in touch, please send an email to <a href=\"mailto:abc@example.com\">Email</a>.</p>")
+}
+
 func main() {
 	http.HandleFunc("/", handlerFunc)
+	http.HandleFunc("/contact", contactHandler)
 	fmt.Println("Server is running on port 3000")
 	http.ListenAndServe(":3000", nil)
 }
