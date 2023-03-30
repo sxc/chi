18a47ee144e169f41e88f1580a033488c19373c1
Jim Shen <3212762+sxc@users.noreply.github.com>
2023-03-30 17:50:10 +0800

    change handle to handlefunc

--- main.go
+++ main.go
@@ -16,9 +16,17 @@ func contactHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprint(w, "<h1>Contact Page</h1><p>To get in touch, please send an email to <a href=\"mailto:abc@example.com\">Email</a>.</p>")
 }
 
+func pathHandler(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path == "/" {
+		homeHandler(w, r)
+	} else if r.URL.Path == "/contact" {
+		contactHandler(w, r)
+	}
+}
+
 func main() {
-	http.HandleFunc("/", homeHandler)
-	http.HandleFunc("/contact", contactHandler)
+	http.HandleFunc("/", pathHandler)
+	// http.HandleFunc("/contact", contactHandler)
 	fmt.Println("Server is running on port 3000")
 	http.ListenAndServe(":3000", nil)
 }
