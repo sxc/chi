5a024bf8f22873118139f89524ec01f22c94391f
Jim Shen <3212762+sxc@users.noreply.github.com>
2023-03-30 17:45:01 +0800

    show path

--- main.go
+++ main.go
@@ -6,8 +6,9 @@ import (
 )
 
 func homeHandler(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "text/html; charset=utf-8")
-	fmt.Fprint(w, "<h1>Hello, World! This is my first Awesome and Great Go web app!</h1>")
+	// w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	// fmt.Fprint(w, "<h1>Hello, World! This is my first Awesome and Great Go web app!</h1>")
+	fmt.Fprint(w, r.URL.Path)
 }
 
 func contactHandler(w http.ResponseWriter, r *http.Request) {
