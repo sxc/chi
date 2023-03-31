3f9a54ddd9ff7f5aad877728b3a2444094912a81
Jim Shen <3212762+sxc@users.noreply.github.com>
2023-03-31 14:16:38 +0800

    add faq template

--- main.go
+++ main.go
@@ -5,43 +5,54 @@ import (
 	"html/template"
 	"log"
 	"net/http"
+	"path/filepath"
 
 	"github.com/go-chi/chi/v5"
 )
 
+// func homeHandler(w http.ResponseWriter, r *http.Request) {
+// 	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+// 	tpl, err := template.ParseFiles("templates/home.gohtml")
+// 	if err != nil {
+// 		log.Printf("parsing template: %v", err)
+// 		http.Error(w, "Internal server error", http.StatusInternalServerError)
+// 		return
+// 	}
+// 	err = tpl.Execute(w, nil)
+// 	if err != nil {
+// 		log.Printf("executing template: %v", err)
+// 		http.Error(w, "Internal server error executing the template.", http.StatusInternalServerError)
+// 	}
+// }
+
 func homeHandler(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "text/html; charset=utf-8")
-	tpl, err := template.ParseFiles("templates/home.gohtml")
-	if err != nil {
-		log.Printf("parsing template: %v", err)
-		http.Error(w, "Internal server error", http.StatusInternalServerError)
-		return
-	}
-	err = tpl.Execute(w, nil)
-	if err != nil {
-		log.Printf("executing template: %v", err)
-		http.Error(w, "Internal server error executing the template.", http.StatusInternalServerError)
-	}
+	tplPath := filepath.Join("templates", "home.gohtml")
+	executeTemplate(w, tplPath)
 }
 
 func contactHandler(w http.ResponseWriter, r *http.Request) {
+	tplPath := filepath.Join("templates", "contact.gohtml")
+	executeTemplate(w, tplPath)
+}
+
+func faqHandler(w http.ResponseWriter, r *http.Request) {
+	executeTemplate(w, filepath.Join("templates", "faq.gohtml"))
+}
+
+func executeTemplate(w http.ResponseWriter, filepath string) {
 	w.Header().Set("Content-Type", "text/html; charset=utf-8")
-	tpl, err := template.ParseFiles("templates/contact.gohtml")
+	tpl, err := template.ParseFiles(filepath)
 	if err != nil {
-		log.Printf("parsing template: %v", err)
-		http.Error(w, "Internal server error", http.StatusInternalServerError)
+		log.Printf("executing template: %v", err)
+		http.Error(w, "Internal server error executing the template.", http.StatusInternalServerError)
 		return
 	}
 	err = tpl.Execute(w, nil)
 	if err != nil {
 		log.Printf("executing template: %v", err)
 		http.Error(w, "Internal server error executing the template.", http.StatusInternalServerError)
+		return
 	}
-}
-
-func faqHandler(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "text/html; charset=utf-8")
-	fmt.Fprint(w, "<h1>FAQ Page</h1><p>FAQ Page2</p>")
 }
 
 func main() {
