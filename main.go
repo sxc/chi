5dc53b9f166f7e7e8a9a922c6c9f1dc2bc0ac845
Jim Shen <3212762+sxc@users.noreply.github.com>
2023-03-31 08:48:59 +0800

    add Router struct

--- main.go
+++ main.go
@@ -16,22 +16,22 @@ func contactHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprint(w, "<h1>Contact Page</h1><p>To get in touch, please send an email to <a href=\"mailto:abc@example.com\">Email</a>.</p>")
 }
 
-func pathHandler(w http.ResponseWriter, r *http.Request) {
-	switch r.URL.Path {
-	case "/":
-		homeHandler(w, r)
-	case "/contact":
-		contactHandler(w, r)
-	default:
-		w.WriteHeader(http.StatusNotFound)
-		fmt.Fprint(w, "<h1>404 Page Not Found</h1><p>Sorry, but the page you were trying to view does not exist.</p>")
-		http.Error(w, "404 Page Not Found", http.StatusNotFound)
-	}
-}
+// func pathHandler(w http.ResponseWriter, r *http.Request) {
+// 	switch r.URL.Path {
+// 	case "/":
+// 		homeHandler(w, r)
+// 	case "/contact":
+// 		contactHandler(w, r)
+// 	default:
+// 		// w.WriteHeader(http.StatusNotFound)
+// 		// fmt.Fprint(w, "<h1>404 Page Not Found</h1><p>Sorry, but the page you were trying to view does not exist.</p>")
+// 		http.Error(w, "404 Page Not Found", http.StatusNotFound)
+// 	}
+// }
 
 func main() {
 	var router Router
-	http.HandleFunc("/", pathHandler)
+	// http.HandleFunc("/", pathHandler)
 	fmt.Println("Server is running on port 3000")
 	http.ListenAndServe(":3000", router)
 }
@@ -45,8 +45,8 @@ func (router Router) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	case "/contact":
 		contactHandler(w, r)
 	default:
-		w.WriteHeader(http.StatusNotFound)
-		fmt.Fprint(w, "<h1>404 Page Not Found</h1><p>Sorry, but the page you were trying to view does not exist.</p>")
+		// w.WriteHeader(http.StatusNotFound)
+		// fmt.Fprint(w, "<h1>404 Page Not Found</h1><p>Sorry, but the page you were trying to view does not exist.</p>")
 		http.Error(w, "404 Page Not Found", http.StatusNotFound)
 	}
 }
