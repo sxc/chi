1dd2dfd37bef8f21fe99c10d1a408aa99e936b4d
Jim Shen <3212762+sxc@users.noreply.github.com>
2023-04-04 20:55:33 +0800

    Sytled the Signup Form

--- main.go
+++ main.go
@@ -22,6 +22,9 @@ func main() {
 	r.Get("/faq", controllers.StaticHandler(
 		views.Must(views.ParseFS(templates.FS, "faq.gohtml", "tailwind.gohtml"))))
 
+	r.Get("/signup", controllers.StaticHandler(
+		views.Must(views.ParseFS(templates.FS, "signup.gohtml", "tailwind.gohtml"))))
+
 	r.NotFound(func(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, "404 Page Not Found", http.StatusNotFound)
 	})
