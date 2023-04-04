cde28aff67f581deb858039ee4f463b1f3a26201
Jim Shen <3212762+sxc@users.noreply.github.com>
2023-04-04 22:44:30 +0800

    Create the users contoller and moved the signup page over to it.

--- main.go
+++ main.go
@@ -22,12 +22,18 @@ func main() {
 	r.Get("/faq", controllers.StaticHandler(
 		views.Must(views.ParseFS(templates.FS, "faq.gohtml", "tailwind.gohtml"))))
 
-	r.Get("/signup", controllers.StaticHandler(
-		views.Must(views.ParseFS(templates.FS, "signup.gohtml", "tailwind.gohtml"))))
+	usersC := controllers.Users{}
+	usersC.Templates.New = views.Must(views.ParseFS(templates.FS,
+		"signup.gohtml", "tailwind.gohtml"))
+	r.Get("/signup", usersC.New)
+
+	// r.Get("/signup", controllers.StaticHandler(
+	// 	views.Must(views.ParseFS(templates.FS, "signup.gohtml", "tailwind.gohtml"))))
 
 	r.NotFound(func(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, "404 Page Not Found", http.StatusNotFound)
 	})
 	fmt.Println("Server is running on port 3000")
+
 	http.ListenAndServe(":3000", r)
 }
