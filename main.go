366deb323050faa08ed6f6c9487b9118ef32200a
Jim Shen <3212762+sxc@users.noreply.github.com>
2023-04-05 21:41:40 +0800

    add postgres config

--- main.go
+++ main.go
@@ -14,8 +14,31 @@ import (
 	_ "github.com/jackc/pgx/v4/stdlib"
 )
 
+type PostgresConfig struct {
+	Host     string
+	Port     string
+	User     string
+	Password string
+	Database string
+	SSLMode  string
+}
+
+func (cfg PostgresConfig) String() string {
+	return fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=%s",
+		cfg.Host, cfg.Port, cfg.User, cfg.Password, cfg.Database, cfg.SSLMode)
+}
+
 func main() {
-	db, err := sql.Open("pgx", "host=localhost port=5432 user=oishifooduser password=oishifoodpassword dbname=oishifooddb sslmode=disable")
+	cfg := PostgresConfig{
+		Host:     "localhost",
+		Port:     "5432",
+		User:     "oishifooduser",
+		Password: "oishifoodpassword",
+		Database: "oishifooddb",
+		SSLMode:  "disable",
+	}
+
+	db, err := sql.Open("pgx", cfg.String())
 	if err != nil {
 		panic(err)
 	}
